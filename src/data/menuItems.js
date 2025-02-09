7c05edb56a744ac99c513ac16105b31341f561e0
furkan salkin <furks3er@gmail.com>
2025-02-09 22:48:11 +0300

    Menü fiyatları ve ürün isimleri güncellendi

--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -1,13 +1,15 @@
 export const menuItems = [
-  { id: 1, name: '1 Porsiyon Köfte', price: 220 },
-  { id: 2, name: '1.5 Porsiyon Köfte', price: 310 },
-  { id: 3, name: 'Duble Köfte', price: 440 },
-  { id: 4, name: '1 Porsiyon Piyaz', price: 160 },
-  { id: 5, name: 'Az Piyaz', price: 90 },
-  { id: 6, name: '1.5 Porsiyon Piyaz', price: 240 },
+  { id: 1, name: '1 Köfte', price: 220 },
+  { id: 2, name: '1.5 Köfte', price: 330 },
+  { id: 3, name: 'Köfte A.P', price: 330 },
+  { id: 4, name: 'Piyaz', price: 150 },
+  { id: 5, name: 'Az Piyaz', price: 110 },
+  { id: 6, name: '1.5 Piyaz', price: 240 },
   { id: 7, name: 'Kutu Kola', price: 50 },
   { id: 8, name: 'Şişe Kola', price: 40 },
-  { id: 9, name: 'Ayran', price: 40 },
-  { id: 10, name: 'Su', price: 10 },
-  { id: 11, name: 'Çay', price: 10 }
+  { id: 9, name: 'Ayran', price: 30 },
+  { id: 10, name: 'Gazoz', price: 30 },
+  { id: 11, name: 'Şalgam', price: 30 },
+  { id: 12, name: 'Su', price: 10 },
+  { id: 13, name: 'Çay', price: 10 }
 ]; 
